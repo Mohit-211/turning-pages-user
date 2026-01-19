8bf6ec02fd99d65909ac404d0bf00f68d3a632e6
bhavya8221 <bhavyasoni8221@gmail.com>
2026-01-19 17:03:23 +0530

    add tiny mc

--- src/Page/ChapterManager/chapterComponent/chapterEditor.jsx
+++ src/Page/ChapterManager/chapterComponent/chapterEditor.jsx
@@ -1,44 +1,50 @@
-import React from "react";
-
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import React, { useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import "./chapterEditor.scss";
-export default function ChapterEditor({ chapter, content, setContent,}) {
+
+export default function ChapterEditor({ chapter, content, setContent }) {
+  const [editorReady, setEditorReady] = useState(false);
+
   return (
-    <>
-      <div className="editor-wrapper">
-        <CKEditor
-          editor={ClassicEditor}
-          data={content || chapter?.content || ""}
-          config={{
-            toolbar: [
-              "heading",
-              "|",
-              "bold",
-              "italic",
-              "link",
-              "|",
-              "bulletedList",
-              "numberedList",
-              "|",
-              "undo",
-              "redo",
-              "|",
-              "style",
-            ],
-            style: {
-              definitions: [
-                {
-                  name: "Dropcap Paragraph",
-                  element: "p",
-                  classes: ["dropcap"],
-                },
-              ],
-            },
-          }}
-          onChange={(e, editor) => setContent(editor.getData())}
-        />
-      </div>
-    </>
+    <div className="editor-wrapper">
+      {!editorReady && (
+        <div className="editor-loader">
+          ✨ Editor ready to edit...
+        </div>
+      )}
+
+      <Editor
+        apiKey="mibv7kc74dumv3uazcc6tu9xu601iqybxjb0qnglj1fn1258"
+        value={content || chapter?.content || ""}
+        init={{
+          height: 500,
+          menubar: true,
+          branding: false,
+          plugins: "table link image lists media charmap emoticons",
+          toolbar:
+            "undo redo | blocks fontfamily fontsize | bold italic underline forecolor backcolor | " +
+            "alignleft aligncenter alignright alignjustify | bullist numlist | link image media table | removeformat",
+          font_family_formats: `
+            Arial=arial,helvetica,sans-serif;
+            Calibri=calibri,arial,helvetica,sans-serif;
+            Times New Roman=times new roman,times,serif;
+            Georgia=georgia,palatino,serif;
+            Verdana=verdana,geneva,sans-serif;
+            Courier New=courier new,courier,monospace;
+          `,
+          automatic_uploads: true,
+          file_picker_types: "image",
+
+          setup: (editor) => {
+            editor.on("init", () => {
+              setTimeout(() => {
+                setEditorReady(true);
+              }, 2000); // ⏱ 2 seconds pause
+            });
+          },
+        }}
+        onEditorChange={(newContent) => setContent(newContent)}
+      />
+    </div>
   );
 }
